1d0a4fa3d4285ad01c9364fa0d05d998b6825b9e
ElviraSalahova <126708582+ElviraSalahova@users.noreply.github.com>
2023-03-30 23:59:58 +0400

    HM6

--- Program.cs
+++ Program.cs
@@ -1,171 +1,43 @@
-﻿
-// ------------------Task 25-------------------------
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (Задачи, решенные через Math.Pow, не будут считаться правильными, так как задача стоит в том, чтобы написать цикл)
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+﻿// -------------------------Task 1-----------------------------
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
 
-//solution without function
-Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
 
-Console.Write("Введите число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int c = 1;
-if (b<1)
-Console.Write("Число B должно быть натуральным\n");
-else
+Console.Write("Enter of elements number: ");
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
 {
-while (b!=0)
-{
-c = c * a;
-b = b - 1;
-}
-Console.Write($"Число A в натуральной степени B равно {c}");
-}
-
-//Solution with function 
-//-----------------------Method-----------------------------------------
-Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
-
-int Exponent(int numberA, int numberB)
-{
-  int result = 1;
-  for(int i=1; i <= numberB; i++)
-  {
-    result = result * numberA;
-  }
-   
-    return result;
-}
-
-  Console.Write("Введите число A: ");
-  int numberA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int numberB = Convert.ToInt32(Console.ReadLine());
-
-  int exponentiation = Exponent(numberA, numberB);
-  Console.WriteLine("Ответ: " + exponentiation);
-
-
-//--------------------------Task 27---------------------------
-//   Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-
-Console.WriteLine($"\nЗадача 27. Выдаёт сумму цифр в числе");
-System.Console.Write("Enter number N: ");
-int Number = Convert.ToInt32(Console.ReadLine()!);
-
-int SumNumbers (int Number)
-{
-    int counter = Convert.ToString(Number).Length;
-    int advance = 0;
-    int result = 0;
-    
-    for (int i = 0; i < counter; i++)
+    if (arr[i] > 0)
     {
-        advance = Number - Number % 10;
-        result = result + (Number - advance);
-        Number = Number / 10; 
+        count++;
     }
-    return result;
-
-}
-int SumNumber = SumNumbers(Number);
-Console.WriteLine("Sum of Numbers: " + SumNumber);
-
-// --------------------Task 29-------------------------------------
-
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
-
-Console.WriteLine($"\nЗадача 29. Ряд чисел преобразует в массив");
-Console.Write("Введите ряд чисел, разделенных запятой : ");
-string? seriesOfNumbers = Console.ReadLine();
-
-seriesOfNumbers = seriesOfNumbers + ","; 
-
-string RemovingSpaces (string series)
-{
-  string seriesNew = "";
-  for (int i = 0; i < series.Length; i++)
-  {
-    if (series[i] != ' ') 
-    {
-      seriesNew += series[i];
-    }
-  }
-  return seriesNew;
 }
  
-void СheckNumber2 (int  series)
-{
-
-      if (series == '0'||series == '1'||series == '2'
-      ||series == '3'||series == '4'||series == '5'||series == '6'
-      ||series == '7'||series == '8'||series == '9'||series == ','
-      ||series == '-')
-      {
-        
-      }
-      
-        else 
-        {
-          Console.WriteLine($"Ошибка ввода  символа. Вводи цифры.");
-
-        }
-}
-
-int[] ArrayOfNumbers(string seriesNew)
-{ 
-
-  int[] arrayOfNumbers = new int[1];    
-
-  int j =0;
-
-  for (int i = 0; i < seriesNew.Length; i++)
-  {
-    string seriesNew1 = "";
-
-    while (seriesNew[i] != ',' && i < seriesNew.Length)
-    {
-      seriesNew1 += seriesNew[i];
-      СheckNumber2(seriesNew[i]);
-      i++;
-    }
-    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);   
-    if (i < seriesNew.Length-1)
-    {
-      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    
-    }
-    j++;
-  }
-  return arrayOfNumbers;
-}
-
-void PrintArry(int[] coll)
-{
-  int count = coll.Length;
-  int index = 0;
-  Console.Write("[");
-  while(index < count)
-  {
-    Console.Write(coll[index]);
-    index++;
-    if (index < count)
-    {
-      Console.Write(", ");
-    }
-  }
-  Console.Write("]");
-} 
+Console.WriteLine($"Count of elements bigger than 0: {count}");
 
 
-string seriesNew = RemovingSpaces(seriesOfNumbers);
+// --------------------------Task 2 ----------------------------
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
 
-PrintArry(arrayOfNumbers);
+
+Console.WriteLine("Введите переменную b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine()!);
+Console.WriteLine("Введите переменную k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b1 + b2) / (k1 + k2);
+double y = k1 * x + b1;
+
+Console.Write($"\n Точка пересечения двух прямых: [{x},{y}]");
+
+
+
